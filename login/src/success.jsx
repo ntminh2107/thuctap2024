fc49659e4495caf6b11f0f15b9109f38ef423ce5
TowfMinh <vinxen2019@gmail.com>
2024-04-01 15:00:06 +0700

    update

--- login/src/success.jsx
+++ login/src/success.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SuccessPage = () => {
   const location = useLocation();
@@ -9,9 +10,24 @@ const SuccessPage = () => {
 
   return (
     <div>
-      <h1>Welcome, {userName}!</h1>
-      <p>Login Successful. You are now logged in.</p>
-      {/* Add additional content or actions here */}
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            My Awesome App
+          </Link>
+          <div className="navbar-nav ml-auto">
+            <span className="navbar-text mr-3">Hello, {userName}!</span>
+            <a href="" className="nav-link">
+              Logout
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-5">
+        <h1>Welcome, {userName}!</h1>
+        <p>Login Successful. You are now logged in.</p>
+        {/* Add additional content or actions here */}
+      </div>
     </div>
   );
 };
